d0fcc6955ae7f4ec5adc0347d5bd8ed9bafa139c
johnnyoko <33661440+johnnyoko@users.noreply.github.com>
2017-11-14 20:29:16 -0500

    Add files via upload

--- Hurricanes2.java
+++ Hurricanes2.java
@@ -27,8 +27,9 @@ public class Hurricanes2
         int category3 = 2;
         int category4 = 3;
         int category5 = 4;
-           
-        int arrayLength = 59;
+        int arrayLength = 59;   
+        int index = 0;
+        
         int [] year = new int[arrayLength];
         String [] month = new String[arrayLength];
         int [] pressure = new int[arrayLength];
@@ -38,87 +39,83 @@ public class Hurricanes2
         
         File newData = new File("/Users/johnny/Desktop/AP Computer Science/Modules/Module 6/Assignments/6.04 Challenge Program/hurcdata2.txt");
         Scanner inFile = new Scanner(newData);
-        int index = 0;
+        
         while (inFile.hasNext())
         {
-            year[index] = inFile.nextInt();
-            month[index] = inFile.next();
-            pressure[index] = inFile.nextInt();
-            windSpeed[index] = inFile.nextInt();
-            name[index] = inFile.next();
-            
+            inFile.nextLine();
             index++;
+            for (int i = 0; i < 59; i++)
+            {
+                year[index] = inFile.nextInt();
+                month[index] = inFile.next();
+                pressure[index] = inFile.nextInt();
+                windSpeed[index] = Double.parseDouble(inFile.nextDouble());
+                name[index] = inFile.next();
+            
+                windSpeed = windSpeed*1.15077945;
+                if (windData < 95.0)
+                {
+                    category[index] = 1;
+                    category1++;
+                } 
+                else if (windData <= 110) 
+                {
+                    category[index] = 2;
+                    category2++;
+                } 
+                else if (windData <= 129) 
+                {
+                    category[index] = 3;
+                    category3++;
+                } 
+                else if (windData <= 156) 
+                {
+                    category[index] = 4;
+                    category4++;
+                } 
+                else if (windData > 156) 
+                {
+                    category[index] = 5;
+                    category5++;
+                }
+                windSpeed[index] = windData;
+                categoryAverage += category[index];
+                windAverage += windSpeed[index];
+                name[index] = inFile.next();
+                int categoryV2 = category[index];
+                int pressure2 = pressure[index];
+                double windSpeed2 = windSpeed[index];
+                if  (maxCategory < categoryV2) 
+                {
+                    maxCategory = category[index];
+                    minCategory = category[index];
+                }
+                if (minCategory > categoryV2)
+                {
+                    minCategory = category[index];
+                }            
+                if (maxPressure < pressure2) 
+                {
+                    maxPressure = pressure[index];
+                    minPressure = pressure[index];
+                }
+                if (minPressure > pressure2)
+                {
+                    minPressure = pressure[index];
+                }           
+                if (maxWindSpeed < windSpeed2)
+                {
+                    maxWindSpeed = windSpeed[index];
+                    minWindSpeed = windSpeed[index];
+                }
+                if (minWindSpeed > windSpeed2) 
+                {
+                    minWindSpeed = windSpeed[index];
+                }          
+                index++;
+            }
         }
         
-        for (int i = 0; i < 59; i++)
-        {
-            double windData = Double.parseDouble(inFile.next());
-            windData = windData*1.15077945;
-            if (windData < 95.0) 
-            {
-            category[index] = 1;
-            category1++;
-            } 
-            else if (windData <= 110) 
-            {
-                category[index] = 2;
-                category2++;
-            } 
-            else if (windData <= 129) 
-            {
-                category[index] = 3;
-                category3++;
-            } 
-            else if (windData <= 156 ) 
-            {
-                category[index] = 4;
-                category4++;
-            } 
-            else if (windData > 156) 
-            {
-                category[index] = 5;
-                category5++;
-            }
-            windSpeed[index] = windData;
-            categoryAverage += category[index];
-            windAverage += windSpeed[index];
-            name[index] = inFile.next();
-            int categoryV2 = category[index];
-            int pressure2 = pressure[index];
-            double windSpeed2 = windSpeed[index];
-            if  (maxCategory < categoryV2) 
-            {
-                maxCategory = category[index];
-                minCategory = category[index];
-            }
-            if (minCategory > categoryV2) 
-            {
-                minCategory = category[index];
-            }            
-            if (maxPressure < pressure2) 
-            {
-                maxPressure = pressure[index];
-                minPressure = pressure[index];
-            }
-            if (minPressure > pressure2)
-            {
-                minPressure = pressure[index];
-            }           
-            if (maxWindSpeed < windSpeed2) 
-            {
-                maxWindSpeed = windSpeed[index];
-                minWindSpeed = windSpeed[index];
-            }
-            if (minWindSpeed > windSpeed2) 
-            {
-                minWindSpeed = windSpeed[index];
-            }          
-            index++;
-        }
-    
-        //Output - print table using printf to format the columns
-
-
         System.out.println("                      Hurricanes 1980 - 2006");
         System.out.println();
         System.out.println("Year     Hurricane    Category     Pressure (mb)     Wind Speed (mph)");
